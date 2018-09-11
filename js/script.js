eb607aa2ff4972d99e9ef20a930a02e5b4976376
agrnrs <43164370+agrnrs@users.noreply.github.com>
2018-09-11 11:12:56 +0300

    unique num gen

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@
 // lättare att göra unique nums
 // sen nån gång.
 
+
+/*
+//gamla koden
 var j;
 function lottoRad() {
 	for (var i=0; i<=6; i++) {
@@ -14,4 +17,25 @@ function lottoRad() {
 	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
 	}
 	alert("lottoraden klar!");
+}
+*/
+
+var l = 0; //ehrm, ett L, inte en siffra
+function lottoRad() {
+max = 38; //+1
+var nums = [0,0,0,0,0,0,0];
+var j = 0;
+
+nums[0] = Math.floor(Math.random() * max)+1;
+document.getElementById ("lotto_0").innerHTML = nums[0];
+for (var i=1; i<=6; i++){
+	j = Math.floor(Math.random() * max)+1;
+	if (nums.includes(j)) {i--}
+	else (nums[i] = j)
+	document.getElementById ("lotto_" + i).innerHTML = nums[i];
+}
+l++;
+document.getElementById ("loops").innerHTML = l;
+
+// alert("rad ok");
 }
