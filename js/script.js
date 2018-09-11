f38a37e6c4524c970cb0909344e962ce2f7a73c9
agrnrs <43164370+agrnrs@users.noreply.github.com>
2018-09-11 10:40:37 +0300

    alert added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
+
+
+// borde göras med array
+// lättare att göra unique nums
+// sen nån gång.
+
+var j;
 function lottoRad() {
-	for (var i=0; i<=6; i++) {
-
-		let lottery = Math.floor(Math.random() * 38)+1;
+	// init nums
+		for (var i=0; i<=6; i++) {
+		let lottery = 0;
 	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
-
+	
+	// new nums
+	for (var i=0; i<=6; i++) {
+		j = Math.floor(Math.random() * 38)+1;
+		let lottery = j;
+	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
 	}
+	alert("lottoraden klar!");
 }
